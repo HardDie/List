fd039a8f447f1e8d9c5639e8f89b921345abc7af
HardDie <oleg1995sysoev@yandex.ru>
2016-05-31 23:23:10 +0600

    Реализованы функии: очистка, добавить в конец, добавить в начало, вытолкнуть первый, вытолкнуть последний, получить элемент по индексу, удалить элемент по индексу, количество элементов в списке

---
+++ List.c
@@ -0,0 +1,191 @@
+#include "List.h"
+
+/*
+====================
+ListFree
+
+	Очищает память от всех эллементов
+====================
+*/
+int ListFree( usList_t ** list ) {
+	while ( *list != NULL ) {
+		usList_t * tempPoint = *list;
+		*list = (*list)->rightNode_;
+		free( tempPoint->data_ );
+		free( tempPoint );
+	}
+	return 0;
+}
+
+/*
+====================
+ListPushBack
+
+	Добавляет элемент в конец списка
+====================
+*/
+int	ListPushBack( usList_t ** list, void *data ) {
+	if ( (*list) == NULL ) {
+		(*list) = malloc( sizeof( usList_t ) );
+		(*list)->leftNode_ = NULL;
+		(*list)->rightNode_ = NULL;
+		(*list)->data_ = data;
+	} else {
+		usList_t * tempPoint = *list;
+		while ( tempPoint->rightNode_ != NULL ) {
+			tempPoint = tempPoint->rightNode_;
+		}
+		tempPoint->rightNode_ = malloc( sizeof( usList_t ) );
+		tempPoint->rightNode_->leftNode_ = tempPoint;
+		tempPoint->rightNode_->rightNode_ = NULL;
+		tempPoint->rightNode_->data_ = data;
+	}
+	return 0;
+}
+
+/*
+====================
+ListPushFront
+
+	Добавляет элемент в начало списка
+====================
+*/
+int	ListPushFront( usList_t ** list, void *data ) {
+	if ( (*list)->data_ == NULL ) {
+		(*list) = malloc( sizeof( usList_t ) );
+		(*list)->leftNode_ = NULL;
+		(*list)->rightNode_ = NULL;
+		(*list)->data_ = data;
+	} else {
+		(*list)->leftNode_ = malloc( sizeof( usList_t ) );
+		(*list)->leftNode_->leftNode_ = NULL;
+		(*list)->leftNode_->rightNode_ = (*list);
+		(*list)->leftNode_->data_ = data;
+		(*list) = (*list)->leftNode_;
+	}
+	return 0;
+}
+
+/*
+====================
+ListSize
+
+	Возвращает размер списка
+====================
+*/
+int	ListSize( usList_t ** list ) {
+	int i = 0;
+	usList_t * pList = *list;
+	while ( pList != NULL ) {
+		pList = pList->rightNode_;
+		i++;
+	}
+	return i;
+}
+
+/*
+====================
+ListGetElement
+
+	Возвращает элемент под номером numElement, в случае ошибки возвращает NULL
+====================
+*/
+void * ListGetElement( usList_t ** list, int numElement ) {
+	if ( numElement < 0 || numElement >= ListSize( list ) || *list == NULL ) {
+		return NULL;
+	}
+	usList_t * pList = *list;
+	for ( int i = 0; i < numElement; i++ ) {
+		pList = pList->rightNode_;
+	}
+	return pList->data_;
+}
+
+/*
+====================
+ListDeleteElement
+
+	Удаляет элемент под номером numElement, в случае ошибки возвращает -1
+====================
+*/
+int ListDeleteElement( usList_t ** list, int numElement ) {
+	if ( numElement < 0 || numElement >= ListSize( list ) || *list == NULL ) {
+		return -1;
+	}
+	usList_t * pList = (*list);
+	for ( int i = 0; i < numElement; i++ ) {
+		pList = pList->rightNode_;
+	}
+	if ( pList->leftNode_ == NULL && pList->rightNode_ == NULL ) {
+		free( (*list)->data_ );
+		free( (*list) );
+		(*list) = NULL;
+	} else if ( pList->leftNode_ == NULL ) {
+		(*list) = (*list)->rightNode_;
+		free( (*list)->leftNode_->data_ );
+		free( (*list)->leftNode_ );
+		(*list)->leftNode_ = NULL;
+	} else if ( (*list)->rightNode_ == NULL ) {
+		pList->leftNode_->rightNode_ = NULL;
+		free( pList->data_ );
+		free( pList );
+	} else {
+		pList->leftNode_->rightNode_ = pList->rightNode_;
+		free( pList->data_ );
+		free( pList );
+	}
+	return 0;
+}
+
+/*
+====================
+ListPopBack
+
+	Возвращает последний эллемент списка, в случае ошибки возвращает NULL
+====================
+*/
+void * ListPopBack( usList_t ** list ) {
+	if ( *list == NULL ) {
+		return NULL;
+	}
+	usList_t * pList = *list;
+	void * data = NULL;
+	while ( pList->rightNode_ != NULL ) {
+		pList = pList->rightNode_;
+	}
+	if ( pList->leftNode_ == NULL ) {
+		data = (*list)->data_;
+		free( *list );
+		*list = NULL;
+	} else {
+		data = pList->data_;
+		pList->leftNode_->rightNode_ = NULL;
+		free( pList );
+	}
+	return data;
+}
+
+/*
+====================
+ListPopFront
+
+	Возвращает первый эллемент списка, в случае ошибки возвращает NULL
+====================
+*/
+void * ListPopFront( usList_t ** list ) {
+	if ( *list == NULL ) {
+		return NULL;
+	}
+	void * data;
+	if ( (*list)->rightNode_ == NULL ) {
+		data = (*list)->data_;
+		free( *list );
+		*list = NULL;
+	} else {
+		data = (*list)->data_;
+		(*list) = (*list)->rightNode_;
+		free( (*list)->leftNode_ );
+		(*list)->leftNode_ = NULL;
+	}
+	return data;
+}
---
+++ List.h
@@ -0,0 +1,22 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+#include <stdlib.h>
+
+struct usList_t;
+
+typedef struct usList_t {
+	struct usList_t *		leftNode_;
+	struct usList_t *		rightNode_;
+	void *					data_;
+} usList_t;
+
+int			ListFree( usList_t ** list );
+int			ListPushBack( usList_t ** list, void *data );
+int			ListPushFront( usList_t ** list, void *data );
+int			ListSize( usList_t ** list );
+void *		ListGetElement( usList_t ** list, int numElement );
+int			ListDeleteElement( usList_t ** list, int numElement );
+void *		ListPopBack( usList_t ** list );
+void *		ListPopFront( usList_t ** list );
+
+#endif
